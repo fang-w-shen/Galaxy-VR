61bbe9e3722f873ceddec464465a2e24ab8d25b8
Fang <fang-wen.shen@vanderbilt.edu>
2017-12-01 00:21:21 -0600

    test

--- index.vr.js
+++ index.vr.js
@@ -6,25 +6,48 @@ import {
   Text,
   View,
   Model,
+  Animated,
 } from 'react-vr';
+const AnimatedModel = Animated.createAnimatedComponent(Model);
+export default class demo extends React.Component {
+  state = {
+    rotation: new Animated.Value(0)
+  }
 
-render() {
-  return (
-    <View>
-      <Pano source={asset('chess-world.jpg')}/>
-      <Model
-        source={{
-          obj: asset('hashrocket.obj')
-        }}
-        style={{
-          color: '#af1e23',
-          transform: [
-            {translate: [0, -3, -12]}
-          ]
-        }}
-      />
-    </View>
-  );
-}
+  componentDidMount() {
+    this.rotate();
+  }
+
+  rotate = () => {
+    this.state.rotation.setValue(0);
+    Animated.timing(
+      this.state.rotation,
+      {
+        toValue: 360,
+        duration: 10000,
+      }
+    ).start(this.rotate);
+  }
+
+  render() {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        <AnimatedModel
+          source={{
+            obj: asset('untitled.obj')
+          }}
+          style={{
+            color: '#af1e23',
+            transform: [
+              {translate: [0, -3, -12]},
+              {rotateY: this.state.rotation}
+            ]
+          }}
+        />
+      </View>
+    );
+  }
+};
 
 AppRegistry.registerComponent('demo', () => demo);
