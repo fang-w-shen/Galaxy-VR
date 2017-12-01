8f76d0ef50fb95c4eeba75ff7b33466f5ff0d38b
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-30 23:53:46 -0600

    test

--- index.vr.js
+++ index.vr.js
@@ -8,12 +8,14 @@ import {
   Model,
 } from 'react-vr';
 
-export default class demo extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Model source={{obj: asset('untitled.obj')}}
+render() {
+  return (
+    <View>
+      <Pano source={asset('chess-world.jpg')}/>
+      <Model
+        source={{
+          obj: asset('hashrocket.obj')
+        }}
         style={{
           color: '#af1e23',
           transform: [
@@ -21,23 +23,8 @@ export default class demo extends React.Component {
           ]
         }}
       />
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
-  }
-};
+    </View>
+  );
+}
 
 AppRegistry.registerComponent('demo', () => demo);
